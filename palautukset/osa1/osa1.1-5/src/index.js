1dd9ed7043ecd5816897b50c8a2785f194303c3b
Ismo Aulaskari <ismo@aulaskari.com>
2018-01-24 17:58:00 +0200

    tehtävä 1.5 ml. outoja konsolivaroituksia Each child in an array or iterator should have a unique key prop.

--- palautukset/osa1/osa1.1-5/src/index.js
+++ palautukset/osa1/osa1.1-5/src/index.js
@@ -37,27 +37,28 @@ const Yhteensa = (props) => {
 }
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osat = [
-        {
-            nimi: 'Reactin perusteet',
-            tehtavia: 10
-        },
-        {
-            nimi: 'Tiedonvälitys propseilla',
-            tehtavia: 7
-        },
-        {
-            nimi: 'Komponenttien tila',
-            tehtavia: 14
-        }
-    ]
-
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: 'Reactin perusteet',
+                tehtavia: 10
+            },
+            {
+                nimi: 'Tiedonvälitys propseilla',
+                tehtavia: 7
+            },
+            {
+                nimi: 'Komponenttien tila',
+                tehtavia: 14
+            }
+        ]
+    }
     return (
         <div>
-            <Otsikko kurssi={kurssi} />
-            <Sisalto data={osat} />
-            <Yhteensa counts={osat} />
+            <Otsikko kurssi={kurssi.nimi} />
+            <Sisalto data={kurssi.osat} />
+            <Yhteensa counts={kurssi.osat} />
         </div>
     )
 }
