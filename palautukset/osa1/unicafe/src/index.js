03296d61172feb0004e297beae7ed44efa0dd375
Ismo Aulaskari <ismo@aulaskari.com>
2018-01-25 16:27:29 +0200

    tehtävä 1.7

--- palautukset/osa1/unicafe/src/index.js
+++ palautukset/osa1/unicafe/src/index.js
@@ -4,25 +4,25 @@ const intro = "Arvostele Unicafe"
 const tulos = "Tulokset:"
 
 class App extends React.Component {
-    
+
     constructor(props) {
         super(props)
         this.state = {
             hyva: 0,
             neutraali: 0,
-            huono: 0            
+            huono: 0
         }
     }
 
     klikHyva = () => {
         this.setState({
-            hyva: this.state.hyva + 1            
+            hyva: this.state.hyva + 1
         })
     }
 
     klikNeutraali = () => {
         this.setState({
-            neutraali: this.state.neutraali + 1            
+            neutraali: this.state.neutraali + 1
         })
     }
 
@@ -32,20 +32,36 @@ class App extends React.Component {
         })
     }
 
-    render() {        
+    render() {
+        const keskiarvo = () => {
+            let aania = this.state.huono + this.state.neutraali + this.state.hyva
+            return (this.state.huono * -1 + this.state.neutraali * 0 + this.state.hyva * 1) / aania
+        }
+        const positiivisia = () => {
+            let aania = this.state.huono + this.state.neutraali + this.state.hyva
+            return (this.state.hyva / aania)
+        }        
         return (
             <div>
                 <h1>{intro}</h1>
-                <div>          
+                <div>
                     <button onClick={this.klikHuono}>huono</button>
                     <button onClick={this.klikNeutraali}>neutraali</button>
                     <button onClick={this.klikHyva}>hyva</button>
-                    <br/>       
-                    <h3>{tulos}</h3>             
-                    <br/>Huono {this.state.huono}
-                    <br/>Neutraali {this.state.neutraali}
-                    <br/>hyva {this.state.hyva}
-                    
+                    <br />
+                    <h3>{tulos}</h3>
+                    <br />Huono {this.state.huono}
+                    <br />Neutraali {this.state.neutraali}
+                    <br />hyva {this.state.hyva}
+                    <br/>
+                    <div>
+                    <h3>Keskiarvo</h3>
+                    {keskiarvo()}
+                    </div>
+                    <div>
+                    <h3>Positiivisia</h3>
+                    {positiivisia()}%
+                    </div>                    
                 </div>
             </div>
         )
