c48b23cd3b4d6b754a46d8a024e797ba50505aa8
Ismo Aulaskari <ismo@aulaskari.com>
2018-01-28 22:30:56 +0200

    Tehtävä 2.4

--- palautukset/osa2/osa2.1-5/src/App.js
+++ palautukset/osa2/osa2.1-5/src/App.js
@@ -2,26 +2,49 @@ import React from 'react'
 import Kurssi from './components/Kurssi'
 
 const App = () => {
-    const kurssi = {
+    const kurssit = [
+      {
         nimi: 'Half Stack -sovelluskehitys',
+        id: 1,
         osat: [
-            {
-                nimi: 'Reactin perusteet',
-                tehtavia: 10
-            },
-            {
-                nimi: 'Tiedonvälitys propseilla',
-                tehtavia: 7
-            },
-            {
-                nimi: 'Komponenttien tila',
-                tehtavia: 14
-            }
+          {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10,
+            id: 1
+          },
+          {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7,
+            id: 2
+          },
+          {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14,
+            id: 3
+          }
         ]
-    }
+      },
+      {
+        nimi: 'Node.js',
+        id: 2,
+        osat: [
+          {
+            nimi: 'Routing',
+            tehtavia: 3,
+            id: 1
+          },
+          {
+            nimi: 'Middlewaret',
+            tehtavia: 7,
+            id: 2
+          }
+        ]
+      }
+    ]
+  
     return (
         <div>
-            <Kurssi kurssi={kurssi} />       
+            {kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi} />)}       
         </div>
     )
 }
