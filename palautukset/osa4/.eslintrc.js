7808c2e94c1c052752c58d932bdea8142b7facd6
Ismo Aulaskari <ismo@aulaskari.com>
2018-02-16 20:54:21 +0200

    Tehtävä 4.3

--- palautukset/osa4/.eslintrc.js
+++ palautukset/osa4/.eslintrc.js
@@ -1,8 +1,15 @@
 module.exports = {
-    "globals": { "process": false, "global": false, "module": false },
+    "globals": {
+        "process": false,
+        "global": false,
+        "module": false,
+        "test": true,
+        "expect": true,
+        "describe": true
+    },
     "env": {
         "browser": true,
-         "amd": true,
+        "amd": true,
         "es6": true
     },
     "extends": "eslint:recommended",
@@ -24,13 +31,13 @@ module.exports = {
             "never"
         ],
         "eqeqeq": "error",
- "no-trailing-spaces": "error",
-  "object-curly-spacing": [
-      "error", "always"
-  ],
-  "arrow-spacing": [
-      "error", { "before": true, "after": true }
-  ],
-  "no-console": 0
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "no-console": 0
     }
 };
