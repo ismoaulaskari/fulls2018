082facc9e1c2c43eefb544a93324f47197500248
Ismo Aulaskari <ismo@aulaskari.com>
2018-02-17 19:52:48 +0200

    Tehtävä 4.4

--- palautukset/osa4/utils/list_helper.js
+++ palautukset/osa4/utils/list_helper.js
@@ -3,6 +3,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue.likes
+  let total = blogs.reduce(reducer, 0)
+  return total
+}
+
 module.exports = {
-  dummy
+  dummy,totalLikes
 }
