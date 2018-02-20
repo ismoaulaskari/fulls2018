3982ec40e72410d2e272d5e64677a95111502294
Ismo Aulaskari <ismo@aulaskari.com>
2018-02-20 23:38:22 +0200

    toimis mutta paluudata ei löydy

--- palautukset/osa4/tests/blogs_api.test.js
+++ palautukset/osa4/tests/blogs_api.test.js
@@ -2,13 +2,34 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+const hogan = {
+  'title': 'My life',
+  'author': 'Hulk Hogan',
+  'url': 'http://localhost',
+  'likes': 0
+}
 
-afterAll(() => {
-  server.close()
+
+describe('api level tests', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('posted blog is returned with id', async () => {
+    const response = await api
+      .post('/api/blogs')
+      .send(hogan)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    //console.log(response)
+    const contents = response.body.map(r => r.content)
+    expect(contents).toContain('"id:"')
+  })
+
+  afterAll(() => {
+    server.close()
+  })
 })
