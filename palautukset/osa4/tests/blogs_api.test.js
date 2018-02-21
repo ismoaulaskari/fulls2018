ee0bb72343f9f14038a61ccfb8eee17a3fc870d0
Ismo Aulaskari <ismo@aulaskari.com>
2018-02-21 20:25:03 +0200

    Tehtävä 4.9 testi valmis

--- palautukset/osa4/tests/blogs_api.test.js
+++ palautukset/osa4/tests/blogs_api.test.js
@@ -18,15 +18,22 @@ describe('api level tests', () => {
       .expect('Content-Type', /application\/json/)
   })
 
-  test('posted blog is returned with id', async () => {
-    const response = await api
+  test('posted blog is in db and returned with id', async () => {
+    const postresponse = await api
       .post('/api/blogs')
       .send(hogan)
       .expect(201)
       .expect('Content-Type', /application\/json/)
-    //console.log(response)
-    const contents = response.body.map(r => r.content)
-    expect(contents).toContain('"id:"')
+
+    const allblogs = await api.get('/api/blogs')
+    const found = allblogs.body.find(r => r.title === hogan.title)
+    expect(found.author).toContain(hogan.author)
+    //expect(allblogs).toContainEqual(expect.objectContaining(postresponse.body))
+
+    const responseblog = postresponse.body
+    expect(responseblog.author).toContain(hogan.author)
+    expect(responseblog.title).toContain(hogan.title)
+    expect(responseblog.id).toBeDefined()
   })
 
   afterAll(() => {
