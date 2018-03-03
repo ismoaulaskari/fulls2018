ad729d0fe78deb0d446cfe8b33bca07b3c42dd31
Ismo Aulaskari <ismo@aulaskari.com>
2018-03-03 12:44:20 +0200

    Tehtävä 5.13

--- palautukset/osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ palautukset/osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -22,4 +22,23 @@ describe.only('<SimpleBlog />', () => {
     const likeDiv = blogComponent.find('.likecontent')
     expect(likeDiv.text()).toContain(`${blog.likes} likes`)
   })
+
+  it('2 button clicks cause two actions', () => {
+    const blog = {
+      title: 'My simple blog',
+      author: 'Hulk Hogan',
+      url: 'http://www.google.com',
+      likes: 100
+    }
+
+    const mockHandler = jest.fn()
+    const blogComponent =
+      shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+      const button = blogComponent.find('button')
+      button.simulate('click')
+      button.simulate('click')
+    
+      expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
