7fd6137a091dbd6fe5d698037f08e0c036d82d73
Ismo Aulaskari <ismo@aulaskari.com>
2018-03-11 12:58:24 +0200

    Tehtävä 6.17

--- palautukset/osa6/routed-anecdotes/src/App.js
+++ palautukset/osa6/routed-anecdotes/src/App.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
+const findAnecdoteById = (anecdotes, id) => {
+  return anecdotes.find(a => a.id === id)
+}
+
+const Anecdote = ({anecdote}) => {
+  return (
+  <p>{anecdote.author}: <i>{anecdote.content}</i>
+  <br/>{anecdote.info}, {anecdote.votes} votes</p>
+  )
+}
+
 const Menu = ({ anecdotes, addNew }) => (
   <Router>
     <div>
@@ -10,124 +21,126 @@ const Menu = ({ anecdotes, addNew }) => (
       <Link to="/about">about</Link>&nbsp;
     </div>
       <div>
-        <Route path="/anecdotes" render={() => <AnecdoteList anecdotes={anecdotes} />} />
+        <Route exact path="/anecdotes" render={() => <AnecdoteList anecdotes={anecdotes} />} />
         <Route path="/create_new" render={() => <CreateNew addNew={addNew} />} />
         <Route path="/about" render={() => <About />} />
         <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
+        <Route exact path="/anecdotes/:id" render={({ match }) =>
+          <Anecdote anecdote={findAnecdoteById(anecdotes, match.params.id)}/> } />
       </div>
     </div>
   </Router>
-)
-
-const AnecdoteList = ({ anecdotes }) => (
+    )
+    
+const AnecdoteList = ({anecdotes}) => (
   <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
-    </ul>
-  </div>
-)
-
-const About = () => (
+      <h2>Anecdotes</h2>
+      <ul>
+        {anecdotes.map(anecdote => <li key={anecdote.id} ><Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link></li>)}
+      </ul>
+    </div>
+    )
+    
+    const About = () => (
   <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
+      <h2>About anecdote app</h2>
+      <p>According to Wikipedia:</p>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+      <em>An anecdote is a brief, revealing account of an individual person or an incident.
+        Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+        such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
       An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
-)
-
-const Footer = () => (
+      <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+    </div>
+    )
+    
+    const Footer = () => (
   <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
-
+      Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
+  
     See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code.
   </div>
-)
-
+    )
+    
 class CreateNew extends React.Component {
-  constructor() {
+      constructor() {
     super()
     this.state = {
       content: '',
-      author: '',
-      info: ''
-    }
+    author: '',
+    info: ''
   }
+}
 
   handleChange = (e) => {
-    console.log(e.target.name, e.target.value)
-    this.setState({ [e.target.name]: e.target.value })
+      console.log(e.target.name, e.target.value)
+    this.setState({[e.target.name]: e.target.value })
   }
 
   handleSubmit = (e) => {
-    e.preventDefault()
+      e.preventDefault()
     this.props.addNew({
       content: this.state.content,
-      author: this.state.author,
-      info: this.state.info,
-      votes: 0
-    })
-  }
+    author: this.state.author,
+    info: this.state.info,
+    votes: 0
+  })
+}
 
   render() {
     return (
       <div>
-        <h2>create a new anecdote</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            content
+      <h2>create a new anecdote</h2>
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          content
             <input name='content' value={this.state.content} onChange={this.handleChange} />
-          </div>
-          <div>
-            author
+        </div>
+        <div>
+          author
             <input name='author' value={this.state.author} onChange={this.handleChange} />
-          </div>
-          <div>
-            url for more info
+        </div>
+        <div>
+          url for more info
             <input name='info' value={this.state.info} onChange={this.handleChange} />
-          </div>
-          <button>create</button>
-        </form>
-      </div>
+        </div>
+        <button>create</button>
+      </form>
+    </div>
     )
 
   }
 }
 
 class App extends React.Component {
-  constructor() {
+      constructor() {
     super()
 
     this.state = {
       anecdotes: [
         {
-          content: 'If it hurts, do it more often',
-          author: 'Jez Humble',
-          info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-          votes: 0,
-          id: '1'
-        },
+      content: 'If it hurts, do it more often',
+    author: 'Jez Humble',
+    info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+    votes: 0,
+    id: '1'
+  },
         {
-          content: 'Premature optimization is the root of all evil',
-          author: 'Donald Knuth',
-          info: 'http://wiki.c2.com/?PrematureOptimization',
-          votes: 0,
-          id: '2'
-        }
-      ],
-      notification: ''
-    }
+      content: 'Premature optimization is the root of all evil',
+    author: 'Donald Knuth',
+    info: 'http://wiki.c2.com/?PrematureOptimization',
+    votes: 0,
+    id: '2'
   }
+],
+notification: ''
+}
+}
 
   addNew = (anecdote) => {
-    anecdote.id = (Math.random() * 10000).toFixed(0)
-    this.setState({ anecdotes: this.state.anecdotes.concat(anecdote) })
+      anecdote.id = (Math.random() * 10000).toFixed(0)
+    this.setState({anecdotes: this.state.anecdotes.concat(anecdote) })
   }
 
   anecdoteById = (id) =>
@@ -138,21 +151,21 @@ class App extends React.Component {
 
     const voted = {
       ...anecdote,
-      votes: anecdote.votes + 1
-    }
+    votes: anecdote.votes + 1
+  }
 
-    const anecdotes = this.state.anecdotes.map(a => a.id === id ? voted : a)
+  const anecdotes = this.state.anecdotes.map(a => a.id === id ? voted : a)
 
-    this.setState({ anecdotes })
+    this.setState({anecdotes})
   }
 
   render() {
     return (
       <div>
-        <h1>Software anecdotes</h1>
-        <Menu anecdotes={this.state.anecdotes} addNew={this.addNew} />
-        <Footer />
-      </div>
+      <h1>Software anecdotes</h1>
+      <Menu anecdotes={this.state.anecdotes} addNew={this.addNew} />
+      <Footer />
+    </div>
     );
   }
 }
